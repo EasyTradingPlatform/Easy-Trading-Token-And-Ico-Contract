4374284dae85978597320dc99c029fb96cfdc68d
EasyTradingPlatform <38621514+EasyTradingPlatform@users.noreply.github.com>
2018-06-28 23:19:48 +0300

    Update EasyTradingToken.sol

--- .gitignore/EasyTradingToken.sol
+++ .gitignore/EasyTradingToken.sol
@@ -84,7 +84,7 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract EasyTradingToken is StandardToken { // CHANGE THIS. Update the contract name.
+contract HashnodeTestCoin is StandardToken { // CHANGE THIS. Update the contract name.
 
     /* Public variables of the token */
 
@@ -104,7 +104,7 @@ contract EasyTradingToken is StandardToken { // CHANGE THIS. Update the contract
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function EasyTradingToken() {
+    function HashnodeTestCoin() {
         balances[msg.sender] = 100000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 100000000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "Easy Trading Token";                                   // Set the name for display purposes (CHANGE THIS)
